e64ab361dbcb4b619a53c57e1f7ca193c3997747
Shajidaa <shajidaakterlopa101@gmail.com>
2025-11-09 08:51:09 +0600

    navbar update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -35,32 +35,11 @@ const Navbar = () => {
   );
   return (
     <div className="navbar bg-base-100 shadow-sm">
-      <MyContainer>
+      <MyContainer className={"flex justify-center items-center"}>
         <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                {" "}
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />{" "}
-              </svg>
-            </div>
-            <ul
-              tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            ></ul>
-          </div>
-          <a className=" text-xl">PayUp</a>
+          <Link to={"/"} className="text-xl">
+            PayUp
+          </Link>
         </div>
 
         <div className="navbar-end">
@@ -73,7 +52,7 @@ const Navbar = () => {
               {" "}
               <div
                 data-tip={user.displayName}
-                className="md:size-12 size-8 tooltip  tooltip-bottom border-2 border-[#02471f] rounded-full "
+                className="md:size-12 size-8 tooltip tooltip-bottom border-2 border-[#02471f] rounded-full "
               >
                 <img
                   className="w-full bg-cover h-full rounded-full  "
@@ -101,10 +80,16 @@ const Navbar = () => {
             type="checkbox"
             defaultChecked={localStorage.getItem("theme") === "dark"}
             checked={theme === "dark"}
-            className="toggle theme-controller"
+            className="toggle theme-controller "
           />
         </div>
       </MyContainer>
+
+      <div className="shadow-sm bg-base-100/30 backdrop-blur-md md:hidden fixed bottom-0 left-0 w-full z-50">
+        <div className=" flex justify-center items-center">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+      </div>
     </div>
   );
 };
