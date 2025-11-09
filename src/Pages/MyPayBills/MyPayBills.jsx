cf153dd6934bb09b3ca6f01d27b93a812049c1e5
Shajidaa <shajidaakterlopa101@gmail.com>
2025-11-09 20:36:45 +0600

    pay bills get

--- src/Pages/MyPayBills/MyPayBills.jsx
+++ src/Pages/MyPayBills/MyPayBills.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import { useEffect } from "react";
+import useAxiosSecure from "../../Hooks/Axios/useAxiosSecure";
+import useAuth from "../../Hooks/useAuth";
 
 const MyPayBills = () => {
+  const { user } = useAuth();
+  const axiosInstance = useAxiosSecure();
+
+  useEffect(() => {
+    axiosInstance.get(`/add-bills?email=${user?.email}`).then((data) => {
+      console.log(data);
+    });
+  }, [axiosInstance, user]);
   return <div>pay bills</div>;
 };
 
